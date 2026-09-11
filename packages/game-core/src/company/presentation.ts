576839cc4f14e9d52b39b2696d4c946637f944b6
Vladimir Shelamov <axb48@yandex.ru>
2026-09-11 12:33:10 +0300

    style(f02): apply exact prettier output

--- packages/game-core/src/company/presentation.ts
+++ packages/game-core/src/company/presentation.ts
@@ -23,10 +23,7 @@ function requestedHairStyle(command: CommandOf<'ChangePresentation'>): string {
   const keys = Object.keys(patch);
   requirePhysical(keys.length === 1 && keys[0] === 'hairStyleId', 'INVALID_ARGUMENT');
   const hairStyleId = patch['hairStyleId'];
-  requirePhysical(
-    typeof hairStyleId === 'string' && isEntityId(hairStyleId),
-    'INVALID_ARGUMENT',
-  );
+  requirePhysical(typeof hairStyleId === 'string' && isEntityId(hairStyleId), 'INVALID_ARGUMENT');
   return hairStyleId;
 }
 
